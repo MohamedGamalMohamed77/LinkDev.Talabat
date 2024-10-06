f249aac74a37b9f7222c35efd9a54fc5c3197cda
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-10-06 19:16:59 +0300

    Part 03 Product Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Products/Product.cs
@@ -0,0 +1,24 @@
+﻿using LinkDev.Talabat.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities
+{
+	public class Product : BaseEntity<int>
+	{
+		public required string Name { get; set; }
+		public required string Description { get; set; }
+		public  string? PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+		public int? BrandId { get; set; }
+		public ProductBrand? Brand { get; set; }
+		public int? CategoryId { get; set; }
+		public ProdutCategory? Category { get; set; }
+
+
+	}
+}
