e642e8cbab749f55e8879633432e157f03a3f571
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-10-10 01:45:54 +0300

    Part 04 Service Manager

--- LinkDev.Talabat.Core.Application/DependencyInjection.cs
+++ LinkDev.Talabat.Core.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using LinkDev.Talabat.Core.Aplication.Abstraction.Services;
+using LinkDev.Talabat.Core.Aplication.Abstraction.Services.Products;
 using LinkDev.Talabat.Core.Application.Mapping;
 using LinkDev.Talabat.Core.Application.Services;
+using LinkDev.Talabat.Core.Application.Services.Products;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -11,14 +13,15 @@ using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Core.Application
 {
-	public static class DependencyInjection
+    public static class DependencyInjection
 	{
 		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 		{
 			//services.AddAutoMapper(mapper => mapper.AddProfile(new MappingProfile()));
 			services.AddAutoMapper(typeof(MappingProfile));
 
-			services.AddScoped(typeof(IProductService), typeof(ProductService));
+			//services.AddScoped(typeof(IProductService), typeof(ProductService));
+			services.AddScoped(typeof(IServiceManager), typeof(ServiceManager));
 
 			return services;
 		}
---
+++ LinkDev.Talabat.Core.Application/Services/ServiceManager.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using LinkDev.Talabat.Core.Aplication.Abstraction.Services;
+using LinkDev.Talabat.Core.Aplication.Abstraction.Services.Products;
+using LinkDev.Talabat.Core.Application.Services.Products;
+using LinkDev.Talabat.Core.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Application.Services
+{
+	public class ServiceManager : IServiceManager
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IMapper _mapper;
+		private readonly Lazy<IProductService>_productService;
+
+		public ServiceManager(IUnitOfWork unitOfWork, IMapper mapper)
+		{
+			_unitOfWork = unitOfWork;
+			_mapper = mapper;
+			_productService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));  ;
+		}
+
+		public IProductService ProductService => _productService.Value;
+	}
+}
