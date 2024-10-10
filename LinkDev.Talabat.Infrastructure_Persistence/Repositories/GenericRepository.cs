80f34c94262df2062e2cb2442beb3829960db2a8
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-10-10 10:09:30 +0300

    Part 07 Loading Navigational Properties

--- LinkDev.Talabat.Infrastructure_Persistence/Repositories/GenericRepository.cs
+++ LinkDev.Talabat.Infrastructure_Persistence/Repositories/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using LinkDev.Talabat.Core.Domain.Common;
 using LinkDev.Talabat.Core.Domain.Contracts;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
 using LinkDev.Talabat.Infrastructure.Persistence.Data;
 using System;
 using System.Collections.Generic;
@@ -13,14 +14,22 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Repositories
 		where TEntity : BaseEntity<TKey>
 		where TKey : IEquatable<TKey>
 	{
-		public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false) => withTracking ?
-			await _dbContext.Set<TEntity>().ToListAsync() : await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
-
+		public async Task<IEnumerable<TEntity>> GetAllAsync(bool withTracking = false)
+		{
+		    if (typeof(TEntity) == typeof(Product))
+		    return withTracking ?
+		    	(IEnumerable<TEntity>)await _dbContext.Set<Product>().Include(p => p.Brand).Include(p => p.Category).ToListAsync() :
+		    	(IEnumerable<TEntity>)await _dbContext.Set<Product>().Include(p => p.Brand).Include(p => p.Category).AsNoTracking().ToListAsync();
+		    
+		    return withTracking ?
+		    	await _dbContext.Set<TEntity>().ToListAsync() :
+				await _dbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+		}
 		public async Task<TEntity?> GetAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
-		public async Task AddAsync(TEntity entity)=>await _dbContext.AddAsync(entity);
-		
-		public void Delete(TEntity entity)=>_dbContext.Remove(entity);
-		public void Update(TEntity entity)=>_dbContext.Update(entity);
-		
+		public async Task AddAsync(TEntity entity) => await _dbContext.AddAsync(entity);
+
+		public void Delete(TEntity entity) => _dbContext.Remove(entity);
+		public void Update(TEntity entity) => _dbContext.Update(entity);
+
 	}
 }
