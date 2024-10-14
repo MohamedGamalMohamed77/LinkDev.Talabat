ae01cb05333dcf26ba99c92f9df330d29f221090
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-10-14 20:35:32 +0300

    Part 01 Error Types

---
+++ LinkDev.Talabat.APIs.Controllers/Controllers/Buggy/BuggyController.cs
@@ -0,0 +1,46 @@
+﻿using LinkDev.Talabat.APIs.Controllers.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.APIs.Controllers.Controllers.Buggy
+{
+	public class BuggyController : BaseApiController
+	{
+		[HttpGet("notfound")]
+		public IActionResult GetNotFoundRequest()
+		{ 
+		 return NotFound(new { StatusCode = 404, Messaage = "Not Found" });
+		}
+
+		[HttpGet("badrequest")]
+		public IActionResult GetBadRequest()
+		{
+			return BadRequest(new {StatusCode=400 , Messaage="Bad Request" });
+		}
+
+		[HttpGet("badrequest/{id}")]
+		public IActionResult GetValidation(int id)
+		{
+			return Ok();
+		}
+
+		[HttpGet("servererror")]
+		public IActionResult GetServerError()
+		{
+			throw new Exception();
+		}
+
+		[HttpGet("unauthorized")]
+		public IActionResult GetUnauthorizedError()
+		{
+			return Unauthorized(new { StatusCode = 401, Messaage = "Unauthorized" });
+		}
+
+
+
+	}
+}
