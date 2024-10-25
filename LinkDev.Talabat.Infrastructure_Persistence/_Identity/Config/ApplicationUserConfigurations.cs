96822b9621d72803b38d92a63b5d063bf68529b0
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-10-25 12:48:19 +0300

    Part 03 Identity Module - Apply Configurations (Code Refactor)

--- LinkDev.Talabat.Infrastructure_Persistence/_Identity/Config/ApplicationUserConfigurations.cs
+++ LinkDev.Talabat.Infrastructure_Persistence/_Identity/Config/ApplicationUserConfigurations.cs
@@ -1,4 +1,5 @@
 ﻿using LinkDev.Talabat.Core.Domain.Entities.Identity;
+using LinkDev.Talabat.Infrastructure.Persistence._Common;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace LinkDev.Talabat.Infrastructure.Persistence._Identity.Config
 {
+	[DbContextType(typeof(StoreIdentityDbContext))]
 	public class ApplicationUserConfigurations : IEntityTypeConfiguration<ApplicationUser>
 	{
 		public virtual void Configure(EntityTypeBuilder<ApplicationUser> builder)
