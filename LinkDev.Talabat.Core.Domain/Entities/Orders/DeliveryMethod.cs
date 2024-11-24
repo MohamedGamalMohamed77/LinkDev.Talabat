c9713fe285fc353a326ccdb93773cc2f35ae32df
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-11-24 07:46:14 +0200

    Part 03 Order Module - Migrations and Data Seeding

--- LinkDev.Talabat.Core.Domain/Entities/Orders/DeliveryMethod.cs
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/DeliveryMethod.cs
@@ -9,7 +9,7 @@ namespace LinkDev.Talabat.Core.Domain.Entities.Orders
 	public class DeliveryMethod : BaseEntity<int>
 	{
 		public required string ShortName { get; set; }
-		public required string Descryption { get; set; }
+		public required string Description { get; set; }
 		public decimal Cost { get; set; }
 		public required string DeliveryTime { get; set; }
 
--- LinkDev.Talabat.Infrastructure_Persistence/_Data/StoreDbContext.cs
+++ LinkDev.Talabat.Infrastructure_Persistence/_Data/StoreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using LinkDev.Talabat.Core.Domain.Entities.Products;
+﻿using LinkDev.Talabat.Core.Domain.Entities.Orders;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
 using LinkDev.Talabat.Infrastructure.Persistence._Common;
 using System.Reflection;
 
@@ -6,21 +7,21 @@ namespace LinkDev.Talabat.Infrastructure.Persistence.Data
 {
 	public class StoreDbContext : DbContext
 	{
+		
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductBrand> Brands { get; set; }
+
+		public DbSet<ProductCategory> Categories { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderItem> OrderItems { get; set; }
+		public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
 		public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
 		{
-
 		}
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyInformation).Assembly,
 				type => type.GetCustomAttribute<DbContextTypeAttribute>()?.DbContextType == typeof(StoreDbContext));
 		}
-
-
-		public DbSet<Product> Products { get; set; }
-		public DbSet<ProductBrand> Brands { get; set; }
-
-		public DbSet<ProductCategory> Categories { get; set; }
-
 	}
 }
