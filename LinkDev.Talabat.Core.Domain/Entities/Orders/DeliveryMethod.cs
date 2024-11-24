b9cfc42c47b69a8d9038060201be1f0c830ef540
mohamedgamalmohamedabdelnaby <mohamedgamalabdelnaby599@gmail.com>
2024-11-24 02:00:38 +0200

    Part 01 Order Module - Entities

---
+++ LinkDev.Talabat.Core.Domain/Entities/Orders/DeliveryMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Domain.Entities.Orders
+{
+	public class DeliveryMethod :BaseEntity<int>
+	{
+		public required string ShortName { get; set; }
+		public required string Descryption { get; set; }
+		public decimal Cost { get; set; }
+		public required string DeliveryTime { get; set; }
+
+
+	}
+}
